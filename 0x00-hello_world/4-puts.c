c980ca0cca1ebd54b0f15542b69ac4dda8bdfa18
Miles Ryker Odazie <milesodazie@gmail.com>
2023-02-16 06:42:30 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - This is a light program that prints a line with the puts function
+ * Return: When the program runs it returns 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
