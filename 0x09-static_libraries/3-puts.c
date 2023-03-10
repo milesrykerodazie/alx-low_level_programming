785934e8b4815bf218f7918ebe599153c74fec82
Miles Ryker Odazie <milesodazie@gmail.com>
2023-03-10 15:46:25 -0800

    checking code

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
